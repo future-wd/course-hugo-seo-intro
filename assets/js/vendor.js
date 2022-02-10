9c268e22542d8f29443f260edc2bd1388e61642b
sean-pwd <sean@powerfulwebdesign.com.au>
2022-02-09 23:01:05 -0500

    complete

---
+++ assets/js/vendor.js
@@ -0,0 +1,14 @@
+import { 
+  // Alert, 
+  // Button, 
+  // Carousel, 
+  Collapse, 
+  // Dropdown, 
+  // Modal, 
+  // Offcanvas, 
+  // Popover, 
+  // ScrollSpy, 
+  // Tab, 
+  // Toast, 
+  // Tooltip 
+} from 'bootstrap';
